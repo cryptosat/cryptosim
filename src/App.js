dbbe080e0f9572b6b944248a12f592fc06a94b9a
Gil Shotan <gilsho@cs.stanford.edu>
2021-01-21 17:39:36 +0200

    add scaffolding for documentation bar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 // import logo from './logo.svg';
 import Console from "./Console.js"
+import DocumentationBar from "./DocumentationBar.js"
 import WorldMap from "./WorldMap.js"
 import './App.css';
 
 function App() {
   var ascii = [
-"           .       .                   .       .      .     .      ",
-"          .    .         .    .            .     ______            ",
-"      .           .             .               ////////           ",
-"                .    .   ________   .  .      /////////     .    . ",
-"           .            |.____.  /\        ./////////    .         ",
-"    .                 .//      \/  |\     /////////                ",
-"       .       .    .//          \ |  \ /////////       .     .   .",
-"                    ||.    .    .| |  ///////// .     .            ",
-"     .    .         ||           | |//`,/////                .     ",
-"             .       \\        ./ //  /  \/   .                    ",
-"  .                    \\.___./ //\` '   ,_\     .     .           ",
-"          .           .     \ //////\ , /   \                 .    ",
-"                       .    ///////// \|  '  |    .                ",
-"      .        .          ///////// .   \ _ /          .           ",
-"                        /////////                              .   ",
-"                 .   ./////////     .     .                        ",
-"         .           --------   .                  ..             .",
-"  .               .        .         .                       .     "].join('\n')
+  "           .       .                   .       .      .     .      ",
+  "          .    .         .    .            .     ______            ",
+  "      .           .             .               ////////           ",
+  "                .    .   ________   .  .      /////////     .    . ",
+  "           .            |.____.  /\        ./////////    .         ",
+  "    .                 .//      \/  |\     /////////                ",
+  "       .       .    .//          \ |  \ /////////       .     .   .",
+  "                    ||.    .    .| |  ///////// .     .            ",
+  "     .    .         ||           | |//`,/////                .     ",
+  "             .       \\        ./ //  /  \/   .                    ",
+  "  .                    \\.___./ //\` '   ,_\     .     .           ",
+  "          .           .     \ //////\ , /   \                 .    ",
+  "                       .    ///////// \|  '  |    .                ",
+  "      .        .          ///////// .   \ _ /          .           ",
+  "                        /////////                              .   ",
+  "                 .   ./////////     .     .                        ",
+  "         .           --------   .                  ..             .",
+  "  .               .        .         .                       .     "].join('\n')
 return (
     <div className="App">
       <header className="App-header">
-        <Console>
-          {"Welcome to the Cryptosat Simulator v0.1!"}<br />&nbsp;
-          <code><pre>{ascii}</pre></code>
-          <br />Documentation: type "help"<br />&nbsp;<br />
-        </Console>
+        <div>
+          <Console>
+            {"Welcome to the Cryptosat Simulator v0.1!"}<br />&nbsp;
+            <code><pre>{ascii}</pre></code>
+            <br />Documentation: type "help"<br />&nbsp;<br />
+          </Console>
+          <DocumentationBar />
+        </div>
         <WorldMap />
       </header>
     </div>
---
+++ src/DocumentationBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class DocumentationBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return <div id="documentationBar">Hello, world!</div>
+  }
+}
+
+export default DocumentationBar;
