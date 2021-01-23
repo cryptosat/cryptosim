d4070a56f64060c18010622168efe5c541107431
Gil Shotan <gilsho@cs.stanford.edu>
2021-01-23 07:01:03 +0200

    fix focus in console

--- src/Console.js
+++ src/Console.js
@@ -17,33 +17,39 @@ class Console extends React.Component {
       return;
     }
     event.preventDefault();
-    const command = event.currentTarget.textContent;
+    const content = event.currentTarget.textContent;
     // This is a stub. replace it with the output of a CLI command parser.
     // The CLI command parser should live in a separate class and expose an API that takes a
     // string command and outputs a string response.
-    const response = 'command not found: ' + command.split(' ')[0];
+    const tokens = content.split(' ');
+    const command = tokens[0];
+    var response = 'command not found: ' + command;
+    if (command === 'echo') {
+      response = tokens.slice(1).join(' ');
+    }
     event.currentTarget.textContent = "";
     this.setState({
-      history: [...this.state.history, ['> ' + command, response]],
-      // content: this.state.content + '<p> >' + command + "</p>" + "<p>" + response + "</p>",
+      history: [...this.state.history, [content, response]],
     });
+  }
+
+  focus() {
+    var left = this.console.current.scrollLeft;
+    var top = this.console.current.scrollTop;
+    this.commandLine.current.focus({'preventScroll': true});
   }
 
   render() {
     return (
-        <div ref={this.console} id="console">
+        <div ref={this.console} id="console" onClick={this.focus.bind(this)}>
           {this.props.children}
           {this.state.history.map(function(item, i){
-            return <span key={i} onFocus={console.log('hello')}>{item[0]}<p>{item[1]}</p></span>
+            return <span key={i}>{'> ' + item[0]}<p>{item[1]}</p></span>
           })}
           >&nbsp;
           <span ref={this.commandLine} id='commandline' contentEditable="true" onKeyPress={this.handleKeyPres.bind(this)}></span>
         </div>
     );
-  }
-
-  setFocus(event) {
-    console.log(this.commandLine.current); // .focus();
   }
 
   componentDidUpdate() {
