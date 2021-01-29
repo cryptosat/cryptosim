1e703ebf23d26ea9ce2fe0ae9d17aac04cbade6c
Gil Shotan <gilsho@cs.stanford.edu>
2021-01-29 19:01:11 +0200

    minimize state in Orbit class

--- src/Orbit.js
+++ src/Orbit.js
@@ -1,6 +1,43 @@
 
 import * as satellite from 'satellite.js/lib/index';
 
+function propagateSatellite(satrec, date) {
+  const positionAndVelocity = satellite.propagate(satrec, date);
+  const positionEci = positionAndVelocity.position;
+  const velocityEci = positionAndVelocity.velocity;
+  const gmst = satellite.gstime(date);
+  const positionGd = satellite.eciToGeodetic(positionEci, gmst);
+  const longitudeRad = positionGd.longitude;
+  const latitudeRad  = positionGd.latitude;
+  const height    = positionGd.height;
+
+  return {
+    'longitude': satellite.degreesLong(longitudeRad),
+    'latitude':  satellite.degreesLat(latitudeRad),
+    'altitude': height,
+  }
+}
+
+function angleToObserver(satrec, latitude, longitude, altitude, date) {
+  const positionAndVelocity = satellite.propagate(this.satrec, date);
+  const positionEci = positionAndVelocity.position;
+  const observerGd = {
+    longitude: satellite.degreesToRadians(longitude),
+    latitude: satellite.degreesToRadians(latitude),
+    height: altitude,
+  };
+  const lookAngles = satellite.eciToLookAngles(observerGd, positionEci);
+  const azimuth = lookAngles.azimuth;
+  const elevation = lookAngles.elevation;
+  const rangeSat  = lookAngles.rangeSat;
+  return {
+    'azimuth': azimuth,
+    'elevation': elevation,
+    'rangeSat': rangeSat,
+  }
+}
+
+
 class Orbit {
   constructor(tleLine1, tleLine2) {
     this.satrec = satellite.twoline2satrec(tleLine1, tleLine2);
@@ -8,39 +45,12 @@ class Orbit {
 
   getPosition(date) {
     // var date = new Date();
-    const positionAndVelocity = satellite.propagate(this.satrec, date);
-    const positionEci = positionAndVelocity.position;
-    const velocityEci = positionAndVelocity.velocity;
-    const gmst = satellite.gstime(date);
-    const positionGd = satellite.eciToGeodetic(positionEci, gmst);
-    const longitudeRad = positionGd.longitude;
-    const latitudeRad  = positionGd.latitude;
-    const height    = positionGd.height;
-
-    return {
-      'longitude': satellite.degreesLong(longitudeRad),
-      'latitude':  satellite.degreesLat(latitudeRad),
-      'altitude': height,
-    }
+    return propagateSatellite(this.satrec, date);
   }
 
-  angleToObserver(latitude, longitude, height, date) {
-    const positionAndVelocity = satellite.propagate(this.satrec, date);
-    const positionEci = positionAndVelocity.position;
-    const observerGd = {
-      longitude: satellite.degreesToRadians(longitude),
-      latitude: satellite.degreesToRadians(latitude),
-      height: height
-    };
-    const lookAngles = satellite.eciToLookAngles(observerGd, positionEci);
-    const azimuth = lookAngles.azimuth;
-    const elevation = lookAngles.elevation;
-    const rangeSat  = lookAngles.rangeSat;
-    return {
-      'azimuth': azimuth,
-      'elevation': elevation,
-      'rangeSat': rangeSat,
-    }
+  angleToObserver(latitude, longitude, altitude, date) {
+    return angleToObserver(this.satrec, latitude, longitude,
+      altitude, date);
   }
 
 
